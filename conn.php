4bbc934bc9750059fc63216ed09e46d23b1d9be8
Llambies <admallla@epsg.upv.es>
2019-05-09 16:47:24 +0200

    Update conn.php
    camio de usuario

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 // Variables de conexion
 $dbhost	= "localhost";	   // host
-$dbuser	= "root";		  // nombre de usuario
-$dbpass	= "";		     // contraseña de la base de datos
+$dbuser	= "admal_adrian";		  // nombre de usuario
+$dbpass	= "Puto9008";		     // contraseña de la base de datos
 $dbname	= "pw_grupo7";    // nombre de la base de datos
 ?>
