cfaa0e1c7b406027a89e5b8fcf32cd8af868a8f7
Llambies <admallla@epsg.upv.es>
2019-05-09 15:35:36 +0200

    Update conn.php
    Cambio del nombre a la base de datos

--- conn.php
+++ conn.php
@@ -3,5 +3,5 @@
 $dbhost	= "localhost";	   // host
 $dbuser	= "root";		  // nombre de usuario
 $dbpass	= "";		     // contraseña de la base de datos
-$dbname	= "proyecto";    // nombre de la base de datos
+$dbname	= "pw_grupo7";    // nombre de la base de datos
 ?>
